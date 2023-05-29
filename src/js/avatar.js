5e95ca3ec2269725648098db9ca9cd24ceb7f959
Steven Collins <me@stevenecollins.com>
2023-05-28 21:21:54 -0400

    updated homepage

--- src/js/avatar.js
+++ src/js/avatar.js
@@ -1,48 +1,48 @@
-// // Avatar Stuff
-// let avatar = document.querySelector(".avatar");
-// var closeEyes = document.querySelector(".close");
-// let lookRight = document.querySelector(".lookRight");
-// let lookLeft = document.querySelector(".lookLeft");
-// let lookUp = document.querySelector(".lookUp");
-// let lookDown = document.querySelector(".lookDown");
+// Avatar Stuff
+let avatar = document.querySelector(".avatar");
+var closeEyes = document.querySelector(".close");
+let lookRight = document.querySelector(".lookRight");
+let lookLeft = document.querySelector(".lookLeft");
+let lookUp = document.querySelector(".lookUp");
+let lookDown = document.querySelector(".lookDown");
 
-// // // lookLeft
-// // lookLeft.addEventListener("mouseenter", () => {
-// // 	avatar.style.backgroundPosition = "-328px, 0px";
-// // });
-// // lookLeft.addEventListener("mouseleave", () => {
-// // 	avatar.style.backgroundPosition = "0px, 0px";
-// // });
-
-// // lookDown
-// lookDown.addEventListener("mouseenter", () => {
-// 	avatar.style.backgroundPosition = "-1335px, 0px";
+// // lookLeft
+// lookLeft.addEventListener("mouseenter", () => {
+// 	avatar.style.backgroundPosition = "-328px, 0px";
 // });
-// lookDown.addEventListener("mouseleave", () => {
+// lookLeft.addEventListener("mouseleave", () => {
 // 	avatar.style.backgroundPosition = "0px, 0px";
 // });
 
+// lookDown
+lookDown.addEventListener("mouseenter", () => {
+	avatar.style.backgroundPosition = "-1335px, 0px";
+});
+lookDown.addEventListener("mouseleave", () => {
+	avatar.style.backgroundPosition = "0px, 0px";
+});
 
-// // Start Blinking
-// window.onload = startFunc;
 
-// function startFunc() {
-//     const avatar = document.getElementById('avatarSprite'); // get the avatar 
+// Start Blinking
+window.onload = startFunc;
 
-//     // Start with open eyes
-//     let timer = Math.round(Math.random() * 2000);
+function startFunc() {
+    const avatar = document.getElementById('avatarSprite'); // get the avatar 
 
-//     setInterval(() => {
-//         timer = Math.round(Math.random() * 2000);
-//         // Close eyes
-//         setTimeout(() => {
-//             avatar.style.backgroundPosition = "-166px 0px";
+    // Start with open eyes
+    let timer = Math.round(Math.random() * 2000);
 
-//             setTimeout(() => {
-//                 // Open eyes again
-//                 avatar.style.backgroundPosition = "0px 0px";
-//             }, 150);
-//         }, timer);
-//     }, 7000);
-//     // End Blinking
-// }
+    setInterval(() => {
+        timer = Math.round(Math.random() * 2000);
+        // Close eyes
+        setTimeout(() => {
+            avatar.style.backgroundPosition = "-166px 0px";
+
+            setTimeout(() => {
+                // Open eyes again
+                avatar.style.backgroundPosition = "0px 0px";
+            }, 150);
+        }, timer);
+    }, 7000);
+    // End Blinking
+}
